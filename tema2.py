6977fca5c97338fe610b2d53e39f2915baa1ade8
ionita-maris <ionitamaris@yahoo.com>
2024-10-22 21:40:30 +0300

    tema 2

---
+++ tema2.py
@@ -0,0 +1,11 @@
+articol="”A fi polițist înseamnă a avea o meserie vocațională, deopotrivă interesantă, palpitantă, dinamică, cu intervenții ce implică cunoștințe temeinice și curaj, riscuri dar și satisfacții. Pasiunea și implicarea, corelate cu spiritul de echipă și corectitudinea, sunt calități imperios necesare profilului unui polițist. Toate aceste aspecte, dar și informații despre cum pot deveni polițiști, urmând cursurile unei forme de învățământ din sistemul de apărare, ordine publică şi siguranţă naţională, le-au aflat astăzi, 22 octombrie, elevi din clasele a XI-a și a XII-a din cadrul Colegiului Național „Mihai Eminescu” Baia Mare. Subiectele au fost dezbătute într-o manieră destinsă, elevilor fiindu-le prezentate tehnica și dotările diverselor structuri din cadrul Inspectoratului de Poliție Județean Maramureș”, a informat IPJ Maramureș."
+
+lungime = len(articol)
+parte1 = articol[:lungime // 2]
+parte2 = articol[lungime // 2:]
+parte1 = parte1.strip().upper()
+parte2 = parte2[::-1]
+parte2 = parte2.capitalize()
+parte2 = " ".join(c for c in parte2 if c.isalnum() or c.isspace())
+rezultat = parte1 + " " + parte2
+print(rezultat)
